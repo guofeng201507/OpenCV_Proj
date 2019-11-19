7b061bc5e247615e1a14721b477d1cf06e716c81
guo feng <guofeng20094@gmail.com>
2019-11-19 22:07:15 +0800

    update

--- 02_tesseract_ocr_text/02_test.py
+++ 02_tesseract_ocr_text/02_test.py
@@ -1,13 +1,23 @@
 from PIL import Image
 import pytesseract
+import os
+
+from pathlib import Path
+
+PATH = Path.cwd()
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-path = "text-image.png"
-path_cn = "text-image-cn.png"
+# path = "text-image.png"
+# path_cn = "text-image-cn.png"
+#
+# text_en = pytesseract.image_to_string(Image.open(path))
+# print(text_en)
+#
+# text_cn = pytesseract.image_to_string(Image.open(path_cn), lang='chi_sim')
+# print(text_cn)
 
-text_en = pytesseract.image_to_string(Image.open(path))
+code_path = os.path.join(PATH, 'code_photo/IMG_20191119_215129.jpg')
+
+text_en = pytesseract.image_to_string(Image.open(code_path))
 print(text_en)
-
-text_cn = pytesseract.image_to_string(Image.open(path_cn), lang='chi_sim')
-print(text_cn)
